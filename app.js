4269b129ea13641daeb1613a1af22163935f5dd3
prince <sonykumarprince460@gmail.com>
2023-10-05 15:37:39 +0530

    get api in app

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const cookiParser = require("cookie-parser")
 require('./db/conn');
 
 const port = 8005
+app.get('/',(req,res)=>{
+    res.status(200).json({msg:'okay'})
+})
 
 // middleware
 app.use(express.json());
